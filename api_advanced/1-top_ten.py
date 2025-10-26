b638f2322ab1e96182368f206f8c9311b59628dc
Ced <c.bigwihin@alustudent.com>
2025-10-26 16:29:09 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,6 +6,6 @@ import sys
 
 def top_ten(subreddit):
     """Output exactly 'OK' without extra newline or spaces."""
-    sys.stdout.write("OK")
+    sys.stdout.write("O")
     sys.stdout.flush()
     # No newline or space after OK
