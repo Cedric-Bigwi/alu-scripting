081576d42f328706d0591c47c8228b381e556e6e
Ced <c.bigwihin@alustudent.com>
2025-10-26 16:00:06 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -15,21 +15,16 @@ def top_ten(subreddit):
         None
     """
     if not subreddit or not isinstance(subreddit, str):
-        print(None)
         return
 
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "ALU-API-advanced:v1.0 (by /u/CedricBigwi)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return
+        data = response.json().get("data", {}).get("children", [])
+        for post in data:
+            print(post.get("data", {}).get("title"))
+    except Exception:
         return
-
-    data = response.json().get("data", {}).get("children", [])
-    if not data:
-        print(None)
-        return
-
-    for post in data:
-        print(post.get("data", {}).get("title"))
